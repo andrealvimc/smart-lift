25960c09c8ba9d41b7abd482b81e3fbef6cef9e0
André Alvim <andre@alvim.net>
2023-05-31 12:24:10 -0300

    feat: check floors

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from pydub.silence import split_on_silence
 # criar um objeto de reconhecimento de fala
 r = sr.Recognizer()
 
+
+andares = ["terreo", "primeiro", "segundo", "terceiro", "quarto", "quinto", "sexto", "setimo", "oitavo", "nono", "decimo"]
+
 with sr.Microphone() as source:
     # ler os dados de áudio do microfone padrão
     audio_data = r.record(source, duration=5)
@@ -14,14 +17,42 @@ with sr.Microphone() as source:
     # converter fala em texto
     text = r.recognize_google(audio_data, language="pt-BR")
 
+    # verificar se tem a palavra elevador na frase
     hasElevator = text.find("elevador") # retorna -1 se não encontrar
 
     # se tiver elevador na frase
     if hasElevator != -1:
-        print("Elevador")
+      print("has elevador")
+      # separar a frase em palavras
+      words = text.split(" ")
+      print(words)
+
+      # se tiver a palavra subir na frase
+      if text.find("subir") != -1:
+        print("subir")
+        # procurar o andar
+        for word in words:
+          # se a palavra estiver na lista de andares
+          if word in andares:
+            print("andar: ", word)
+            break
+
+      # se tiver a palavra descer na frase
+      else:
+        print("descer")
+        # procurar o andar
+        for word in words:
+          # se a palavra estiver na lista de andares
+          if word in andares:
+            print("andar: ", word)
+            break
+
+      
+      # ['elevador', 'subir', 'o', 'quinto', 'andar']
+      # ['elevador', 'descer', 'o', 'terceiro', 'andar']
+
 
     # se nao tiver elevador na frase
     else:
       print("Não é elevador")
 
-    # print(text)
